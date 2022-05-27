06d49a1ffba3cfb88b2bb52596bb431f1b6cd229
Christinalu <89390143+Christinalulu@users.noreply.github.com>
2022-05-27 03:01:27 +0200

    coding

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-let searchBtn = document.querySelector("#search-icon");
-let searchBar = document.querySelector(".search-bar-container");
 
-searchBtn.addEventListener("onclick", () => {
-  searchBtn.classList.toggle("e002");
-  searchBar.classList.toggle("active");
-});
